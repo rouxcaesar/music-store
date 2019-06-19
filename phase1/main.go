ae7adba5a47a9ac30711950d90a21064d3418361
Julius Zerwick <jzerwick@digitalocean.com>
2019-06-19 16:51:28 -0400

    Added messages

--- phase1/main.go
+++ phase1/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
   "fmt"
-  "strings"
-  "os"
-  "bufio"
+//  "strings"
+//  "os"
+//  "bufio"
 )
+
+// At start of program, print hello message to user.
+// Wait for user input (add song, delete song, list songs) with a for loop.
+// Execute input and print message.
+// If user inputs "exit" then end for loop and print goodbye message.
 
 func main() {
   // dataStore will store the songs that users provide
@@ -13,10 +18,14 @@ func main() {
   // {
   //   "We Will Rock You": {
   //                         "artist": "Queen",
-  //                         "genre":  "Rock",
-  //                         "year":   "1977"
+  //                         "genre":  "Rock"
   //                       }
   // }
-  var dataStore = map[string]map[string]string{}
+  //var dataStore = map[string]map[string]string{}
+
+  fmt.Println("Welcome to the Music Store!")
+  fmt.Println("1) add a song")
+  fmt.Println("2) delete a song")
+  fmt.Println("3) list songs\n")
 
 }
